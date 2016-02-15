3f0526a5dbded6821da7859cdfe593d5f2802ca5
Daniel Saguez <daniel@saguez.org>
2016-02-15 18:08:16 -0500

    Creado el TextArea y la dependencia a zxing

--- src/main/java/org/saguez/App.java
+++ src/main/java/org/saguez/App.java
@@ -1,29 +1,59 @@
 package org.saguez;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * Swing
  *
  */
 public class App extends JFrame {
-   // Name-constants to define the various dimensions
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5093844796785224031L;
+// Name-constants to define the various dimensions
    public static final int WINDOW_WIDTH = 300;
    public static final int WINDOW_HEIGHT = 150;
  
    // private variables of UI components
     JRadioButton RtoLbutton;
     JRadioButton LtoRbutton;
+    JTextArea cadenaACifrar;
     FlowLayout experimentLayout = new FlowLayout();
     final String RtoL = "Right to left";
     final String LtoR = "Left to right";
     JButton applyButton = new JButton("Apply component orientation");
+	private JButton botonSalir = new JButton("Salir");;
  
-   public App(String name) {
-     super(name);
-   }
    /** Constructor to setup the UI components */
    public App() {
       addWindowListener( new Terminator());
@@ -33,10 +63,9 @@ public class App extends JFrame {
       cp.setLayout(new FlowLayout());
  
       // Allocate the UI components
-      
  
       // Content-pane adds components
-      // cp.add(....)
+      addComponentsToPane(cp);
  
       // Source object adds listener
       // .....
@@ -52,9 +81,49 @@ public class App extends JFrame {
       // Run GUI codes in the Event-Dispatching thread for thread safety
       SwingUtilities.invokeLater(new Runnable() {
          public void run() {
-            new App("QR Seguro Prueba");  // Let the constructor do the job
+            new App();  // Let the constructor do the job
          }
       });
+//////////////////////////////////////////////////////////////////////////////
+//                               QR                                         //
+//////////////////////////////////////////////////////////////////////////////
+      String myCodeText = "http://Crunchify.com/";
+      
+      // change path as per your laptop/desktop location
+      String filePath = "/Users/appshah/Documents/CrunchifyQR.png";
+      int size = 125;
+      String fileType = "png";
+      File myFile = new File(filePath);
+      try {
+          Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+          hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+          QRCodeWriter qrCodeWriter = new QRCodeWriter();
+          BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText,BarcodeFormat.QR_CODE, size, size, hintMap);
+          int CrunchifyWidth = byteMatrix.getWidth();
+          BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+                  BufferedImage.TYPE_INT_RGB);
+          image.createGraphics();
+
+          Graphics2D graphics = (Graphics2D) image.getGraphics();
+          graphics.setColor(Color.WHITE);
+          graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+          graphics.setColor(Color.BLACK);
+
+          for (int i = 0; i < CrunchifyWidth; i++) {
+              for (int j = 0; j < CrunchifyWidth; j++) {
+                  if (byteMatrix.get(i, j)) {
+                      graphics.fillRect(i, j, 1, 1);
+                  }
+              }
+          }
+          ImageIO.write(image, fileType, myFile);
+      } catch (WriterException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      System.out.println("\n\nYou have successfully created QR Code.");
+      
    }
    public void addComponentsToPane(final Container pane) {
         final JPanel compsToExperiment = new JPanel();
@@ -68,13 +137,20 @@ public class App extends JFrame {
         LtoRbutton.setSelected(true);
         RtoLbutton = new JRadioButton(RtoL);
         RtoLbutton.setActionCommand(RtoL);
-         
+        cadenaACifrar = new JTextArea(5,35);
         //Add buttons to the experiment layout
-        compsToExperiment.add(new JButton("Button 1"));
-        compsToExperiment.add(new JButton("Button 2"));
-        compsToExperiment.add(new JButton("Button 3"));
-        compsToExperiment.add(new JButton("Long-Named Button 4"));
-        compsToExperiment.add(new JButton("5"));
+        compsToExperiment.add(cadenaACifrar);
+        compsToExperiment.add(new JButton("2"));
+        compsToExperiment.add(new JButton("3"));
+        compsToExperiment.add(new JButton("4"));
+        compsToExperiment.add(botonSalir);
+
+        botonSalir.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+              System.exit(0);
+            } 
+          } );
+        
         //Left to right component orientation is selected by default
         compsToExperiment.setComponentOrientation(
                 ComponentOrientation.LEFT_TO_RIGHT);
