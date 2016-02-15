27d83069e409a49bd7c05b8fd9cbca59f68a06a4
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2016-02-15 01:31:39 -0500

    Correción

--- src/main/java/org/saguez/App.java
+++ src/main/java/org/saguez/App.java
@@ -12,40 +12,105 @@ public class App extends JFrame {
    // Name-constants to define the various dimensions
    public static final int WINDOW_WIDTH = 300;
    public static final int WINDOW_HEIGHT = 150;
-   // ......
  
    // private variables of UI components
-   // ......
+    JRadioButton RtoLbutton;
+    JRadioButton LtoRbutton;
+    FlowLayout experimentLayout = new FlowLayout();
+    final String RtoL = "Right to left";
+    final String LtoR = "Left to right";
+    JButton applyButton = new JButton("Apply component orientation");
  
+   public App(String name) {
+     super(name);
+   }
    /** Constructor to setup the UI components */
    public App() {
+      addWindowListener( new Terminator());
       Container cp = this.getContentPane();
  
       // Content-pane sets layout
-      // cp.setLayout(new ....Layout());
+      cp.setLayout(new FlowLayout());
  
       // Allocate the UI components
-      // .....
+      
  
       // Content-pane adds components
       // cp.add(....)
  
       // Source object adds listener
       // .....
- 
+
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Exit when close button clicked
-      setTitle("......"); // "this" JFrame sets title
+      setTitle("QR Seguro"); // "this" JFrame sets title
       setSize(WINDOW_WIDTH, WINDOW_HEIGHT);  // or pack() the components
       setVisible(true);   // show it
    }
- 
+
    /** The entry main() method */
    public static void main(String[] args) {
       // Run GUI codes in the Event-Dispatching thread for thread safety
       SwingUtilities.invokeLater(new Runnable() {
          public void run() {
-            new App();  // Let the constructor do the job
+            new App("QR Seguro Prueba");  // Let the constructor do the job
          }
       });
    }
+   public void addComponentsToPane(final Container pane) {
+        final JPanel compsToExperiment = new JPanel();
+        compsToExperiment.setLayout(experimentLayout);
+        experimentLayout.setAlignment(FlowLayout.TRAILING);
+        JPanel controls = new JPanel();
+        controls.setLayout(new FlowLayout());
+         
+        LtoRbutton = new JRadioButton(LtoR);
+        LtoRbutton.setActionCommand(LtoR);
+        LtoRbutton.setSelected(true);
+        RtoLbutton = new JRadioButton(RtoL);
+        RtoLbutton.setActionCommand(RtoL);
+         
+        //Add buttons to the experiment layout
+        compsToExperiment.add(new JButton("Button 1"));
+        compsToExperiment.add(new JButton("Button 2"));
+        compsToExperiment.add(new JButton("Button 3"));
+        compsToExperiment.add(new JButton("Long-Named Button 4"));
+        compsToExperiment.add(new JButton("5"));
+        //Left to right component orientation is selected by default
+        compsToExperiment.setComponentOrientation(
+                ComponentOrientation.LEFT_TO_RIGHT);
+         
+        //Add controls to set up the component orientation in the experiment layout
+        final ButtonGroup group = new ButtonGroup();
+        group.add(LtoRbutton);
+        group.add(RtoLbutton);
+        controls.add(LtoRbutton);
+        controls.add(RtoLbutton);
+        controls.add(applyButton);
+         
+        //Process the Apply component orientation button press
+        applyButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String command = group.getSelection().getActionCommand();
+                //Check the selection
+                if (command.equals("Left to right")) {
+                    compsToExperiment.setComponentOrientation(
+                            ComponentOrientation.LEFT_TO_RIGHT);
+                } else {
+                    compsToExperiment.setComponentOrientation(
+                            ComponentOrientation.RIGHT_TO_LEFT);
+                }
+                //update the experiment layout
+                compsToExperiment.validate();
+                compsToExperiment.repaint();
+            }
+        });
+        pane.add(compsToExperiment, BorderLayout.CENTER);
+        pane.add(controls, BorderLayout.SOUTH); ;
+    }
+}
+
+class Terminator extends WindowAdapter {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0); 
+      }
 }
